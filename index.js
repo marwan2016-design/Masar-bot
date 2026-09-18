af40e43753764386dfae2f9b77761d2509b5fa04
marwan2016-design <marwan2016@gmail.com>
2026-09-19 01:00:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
-const TELEGRAM_TOKEN = '8375130826:AAGCq1il8Bkit0PuvwgZxYakvHssIFC2Wmw';
+const TELEGRAM_TOKEN = '8375130بيب8Bkit0PuvwgZxYakvHssIFC2Wmw';
 const NOCODB_TOKEN = 'nc_pat_malRaeiUmpPk277KG7xUSst1yZBRKue2iuZYvCPv';
 const NOCODB_URL = 'https://app.nocodb.com/api/v2/tables/mpryrwjikak17ao/records';
 
